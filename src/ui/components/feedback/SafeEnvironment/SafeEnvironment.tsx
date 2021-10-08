f27d1dd004cd2bc2f54fd6446d66103ef724a0a2
juliocesar06 <julio06cesar11@gmail.com>
2021-10-08 01:02:09 -0300

    main

---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeEnvironmentContainer} from './SafeEnvironment.styles';
+
+const SafeEnvironment = () =>{
+    return (<SafeEnvironmentContainer>
+                Ambiente Seguro
+            </SafeEnvironmentContainer>);
+};
+
+export default SafeEnvironment;
